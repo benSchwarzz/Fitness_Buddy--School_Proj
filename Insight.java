9069172752ba42ee845951719f806dca27f304c9
Ben Schwarz <147880835+benSchwarzz@users.noreply.github.com>
2024-06-03 14:13:58 -0400

    Finalized Insights
    Finalized insights by adding the insight based on rest time (i.e time between workotus)

--- Insight.java
+++ Insight.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Date;
 
 public class Insight
 {
@@ -9,18 +10,18 @@ public class Insight
         insight4 = "Hard workouts are great, but not too often. Otherwise you will be prone to injuries",
         insight5 = "Try doing a variety of different type of workouts. This will allow you to be more well rounded",
         insight6 = "Not enough workouts logged to make an astute judgement";
-    ArrayList<Workout> workouts;
+    Athlete athlete;
            
-    public ArrayList<String> Insight(ArrayList<Workout> w) {
-        this.workouts = w;
+    public ArrayList<String> Insight(Athlete a) {
+        this.athlete = a;
         ArrayList<String> insights = new ArrayList<String>();
         
-        if (this.workouts.size() > 4) {
-            ///////////////////////////////////////////////////////////
+        if (this.athlete.workouts.size() > 4) {
+            /////////////////////////////////////////////////////////// Versatility
             float end_percent = 0F;
             float hyp_percent = 0F;
             float str_percent = 0F;
-            for (Workout i : this.workouts) {
+            for (Workout i : this.athlete.workouts) {
                 if (i.type == "Endurance") {
                     end_percent += 1;
                 }
@@ -31,38 +32,53 @@ public class Insight
                     str_percent += 1;
                 }
             }
-            end_percent /= this.workouts.size();
-            hyp_percent /= this.workouts.size();
-            str_percent /= this.workouts.size();
+            end_percent /= this.athlete.workouts.size();
+            hyp_percent /= this.athlete.workouts.size();
+            str_percent /= this.athlete.workouts.size();
             
-            if (end_percent > 50 || hyp_percent > 50 || str_percent > 50) {
+            if (end_percent > 70 || hyp_percent > 70 || str_percent > 70) {
                 insights.add(insight5);
             }
-            //////////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////// Difficulty
             float avg_dif = 0;
             
-            for (Workout i : this.workouts) {
+            for (Workout i : this.athlete.workouts) {
                 avg_dif += i.difficulty;
             }
             
-            avg_dif /= this.workouts.size();
+            avg_dif /= this.athlete.workouts.size();
             
             if (avg_dif > 7) {
                 insights.add(insight4);
             }
-            ////////////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////////// Length
             float avg_len = 0;
             
-            for (Workout i : this.workouts) {
+            for (Workout i : this.athlete.workouts) {
                 avg_len += i.length;
             }
             
-            avg_len /= this.workouts.size();
+            avg_len /= this.athlete.workouts.size();
             
             if (avg_len > 75) {
                 insights.add(insight3);
             }
-            ////////////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////////// Rest time
+            athlete.sort("date");
+            long rest;
+            long avg_rest = 0;
+            
+            for (int i = 0; i < this.athlete.workouts.size(); i++) {
+                rest = this.athlete.workouts.get(i).date.getTime() - this.athlete.workouts.get(i + 1).date.getTime();
+                avg_rest += rest;
+            }
+            avg_rest /= this.athlete.workouts.size();
+            
+            if (avg_rest > 2 * 86400000) {
+                insights.add(insight2);
+            } else {
+                insights.add(insight1);
+            }
             
             
         } else {
